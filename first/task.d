e5802c2e2e1ea42fbd2fbbba619a25111495de24
D. Moiseev <isdenmois@gmail.com>
2015-12-15 02:54:37 +0000

    Write task test

--- first/task.d
+++ first/task.d
@@ -20,3 +20,33 @@ class Task {
 		return --this.time;
 	}
 }
+
+unittest {
+	Task t = new Task(3, 10);
+	assert(t.tick == 2);
+	assert(t.tick == 1);
+	assert(t.tick <= 0);
+	assert(t.sigma == 3);
+	assert(t.startTime == 10);
+
+	assert(t.i == 0);
+	assert(new Task(1, 1).i == 1);
+
+	t = new Task(8, 1);
+	assert(t.i == 2);
+
+	Task t1 = new Task(10, 10);
+	assert(t1.i == 3);
+
+	assert(t.sigma == 8);
+	assert(t.tick == 7);
+	assert(t1.sigma == 10);
+
+	t = new Task(1.1, 5);
+	t.tick;
+	assert(t.tick < 0);
+	
+	t = new Task(1.0, 5);
+	t.tick;
+	assert(t.tick <= 0);
+}
