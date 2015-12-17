b1a1b42c9369f15e8155eaaf3d31a633f12c6061
D. Moiseev <isdenmois@gmail.com>
2015-12-17 00:08:09 +0000

    Write list preprocessing function

---
+++ source/utils.d
@@ -0,0 +1,14 @@
+﻿module utils;
+import std.json;
+
+JSONValue convertList(double[] list) {
+	JSONValue[] result;
+	foreach (key, value; list) {
+		result ~= JSONValue([
+			"x": key,
+			"y": value,
+		]);
+	}
+
+	return JSONValue(result);
+}
