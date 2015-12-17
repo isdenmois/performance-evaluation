5fc48024001bab9e796af61b41a9c9178cbec124
D. Moiseev <isdenmois@gmail.com>
2015-12-17 00:22:06 +0000

    Create web-server version

--- source/app.d
+++ source/app.d
@@ -1,22 +1,40 @@
+module main;
 import vibe.web.rest;
 import vibe.d;
 import first.first;
 
 
 // Defines a simple RESTful API
-interface ITest {
+interface PV {
 	@method(HTTPMethod.GET)
 	string computeFirst(uint time, double lambda, double sigma, double mu);
 }
 
 // Local implementation that will be provided by the server
-class Test : ITest {
+class Test : PV {
 	import std.stdio;
 	float computeSum(float a, float b) { return a + b; }
 	void postToConsole(string text) { writeln(text); }
 	string computeFirst(uint time, double lambda, double sigma, double mu) {
 		return first_process(time, lambda, sigma, mu);
 	}
+}
+
+void index(HTTPServerRequest req, HTTPServerResponse res) {
+	string[][] nav = [["Задача 1", "/first"], ["Задача 2", "/second"]];
+	string title = "Методы оценки вычислений";
+	res.render!("index.dt", title, nav);
+}
+
+void first_route(HTTPServerRequest req, HTTPServerResponse res) {
+	string[][] nav = [["Задача 1", "/first"], ["Задача 2", "/second"]];
+	string title = "Задача 1. Простой сервер.";
+	res.render!("first.dt", title, nav);
+}
+
+void bower(scope HTTPServerRequest req, scope HTTPServerResponse res, ref string rr) {
+	import std.stdio;
+	writeln(rr);
 }
 
 shared static this()
@@ -33,11 +51,17 @@ shared static this()
 
 	auto router = new URLRouter;
 	// Serve the generated JavaScript client at /performance_evaluation.js
-	router.get("/performance_evaluation.js", serveRestJSClient!ITest(restsettings));
+	router.get("/performance_evaluation.js", serveRestJSClient!PV(restsettings));
 	// Serve an example page at /
 	// The page will use the test.js script to issue calls to the
 	// REST service.
-	router.get("/", staticTemplate!"index.dt");
+	router.get("/", &index);
+	router.get("/first", &first_route);
+
+	// Serve static files.
+	router.get("*", serveStaticFiles("./node_modules"));
+	router.get("*", serveStaticFiles("./public"));
+
 	// Finally register the REST interface defined above
 	router.registerRestInterface(new Test, restsettings);
 
@@ -48,6 +72,7 @@ shared static this()
 
 	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
 }
+
 void main() {
 	// returns false if a help screen has been requested and displayed (--help)
 	if (!finalizeCommandLineOptions())
