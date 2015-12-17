37dbdd9befde799808bdd8ad0bda2de6c77b2e65
D. Moiseev <isdenmois@gmail.com>
2015-12-17 01:36:48 +0000

    Rewrite constructor for second server

--- source/second/server.d
+++ source/second/server.d
@@ -4,10 +4,16 @@ import second.computer;
 
 class server {
 	Computer[] computers;
+
 	this() {
 		computers ~= Computer(1, 1);
-		computers ~= Computer(2, 1);
-		computers ~= Computer(3, 1);
+		computers ~= Computer(2, 2);
+		computers ~= Computer(3, 3);
 		computers ~= Computer(4, 1);
+		computers ~= Computer(5, 2);
+	}
+
+	void addTask(Task t) {
+
 	}
 }
