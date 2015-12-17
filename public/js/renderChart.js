a8edb0a9cd6ba243b0bdaae91ff2ffa93485a5b3
D. Moiseev <isdenmois@gmail.com>
2015-12-17 00:14:09 +0000

    Write js for first task

---
+++ public/js/renderChart.js
@@ -0,0 +1,22 @@
+﻿function renderChart(selector, title, points) {
+    var chart = new CanvasJS.Chart(selector, {
+      title: {
+        text: title
+      },
+      data: [
+        {
+          color: "#009688",
+          type: points.length < 150 ? "column" : "line",
+          dataPoints: points
+        }
+      ],
+      axisY: {
+        title: 'Вероятность'
+      },
+      axisX: {
+        title: '?'
+      }
+    });
+
+    chart.render()
+}
