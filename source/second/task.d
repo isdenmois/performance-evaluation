27f9dfa2f2a302b570c60b5ca257fd64a1ee5fb6
D. Moiseev <isdenmois@gmail.com>
2015-12-17 01:30:12 +0000

    Write tests for task

--- source/second/task.d
+++ source/second/task.d
@@ -29,10 +29,41 @@ class Task {
 		return null;
 	}
 
+	/**
+	 * Calculate current resource type.
+	 */
 	uint type() {
 		if (this.components.empty) {
 			return 0;
 		}
 		return this.components.front.type;
 	}
+}
+
+unittest {
+	Task t = new Task();
+	t.addComponent(1, 1);
+	t.addComponent(2, 3);
+	t.addComponent(3, 1);
+	t.addComponent(1, 1);
+
+	assert(t.type == 1);
+
+	assert(t.tick !is null);
+	assert(t.type == 2);
+
+	assert(t.tick is null);
+	assert(t.type == 2);
+
+	assert(t.tick is null);
+	assert(t.type == 2);
+
+	assert(t.tick !is null);
+	assert(t.type == 3);
+
+	assert(t.tick !is null);
+	assert(t.type == 1);
+
+	assert(t.tick !is null);
+	assert(t.type == 0);
 }
