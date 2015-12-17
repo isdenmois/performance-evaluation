84169a35c88dae02b7977d42848e8f2ffdb5e60f
D. Moiseev <isdenmois@gmail.com>
2015-12-17 01:31:22 +0000

    Write construtor for second server

--- source/second/server.d
+++ source/second/server.d
@@ -1,10 +1,13 @@
-﻿module source.second.server;
+﻿module source.server;
+import second.task;
+import second.computer;
 
-class server
-{
-	this()
-	{
-		// Constructor code
+class server {
+	Computer[] computers;
+	this() {
+		computers ~= Computer(1, 1);
+		computers ~= Computer(2, 1);
+		computers ~= Computer(3, 1);
+		computers ~= Computer(4, 1);
 	}
 }
-
