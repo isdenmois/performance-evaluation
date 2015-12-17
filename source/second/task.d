787dde6a941f36ab4f3fee58fc87ac9eaeada565
D. Moiseev <isdenmois@gmail.com>
2015-12-17 00:20:57 +0000

    Write task class

--- source/second/task.d
+++ source/second/task.d
@@ -1,10 +1,32 @@
 ﻿module second.task;
 import second.component;
+import std.container.dlist;
 
 class Task {
-	Component[] components;
+	private DList!Component components;
 
+	/**
+	 * Create and insert new component.
+	 */
 	void addComponent(uint resource, double sigma) {
-		this.components ~= new Component(resource, sigma);
+		this.components.insert(new Component(resource, sigma));
+	}
+
+	/**
+	 * Calculate next state.
+	 */
+	Task tick() {
+		if (!this.components.empty) {
+			if (this.components.back.tick <= 0) {
+				this.components.removeBack;
+				return this;
+			}
+		}
+
+		if (this.components.empty) {
+			return this;
+		}
+		return null;
 	}
 }
+
