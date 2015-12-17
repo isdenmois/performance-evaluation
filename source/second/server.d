a123b07bd5f1c71d63d16e3fed8b21e08314d0a7
D. Moiseev <isdenmois@gmail.com>
2015-12-17 02:09:01 +0000

    Write test for second server

--- source/second/server.d
+++ source/second/server.d
@@ -2,7 +2,7 @@
 import second.task;
 import second.computer;
 
-class server {
+class Server {
 	Computer[] computers;
 
 	this() {
@@ -13,7 +13,58 @@ class server {
 		computers ~= new Computer(5, 2);
 	}
 
-	void addTask(Task t) {
+	void addTask(Task task) {
+		uint type = task.type;
+		if (type <= this.computers.length && type > 0) {
+			this.computers[type - 1].addTask(task);
+		}
+	}
+
+	Task[] tick() {
 
 	}
+}
+
+unittest {
+	Server s = new Server();
+
+	Task t1 = new Task();
+	t1.addComponent(1, 2);
+	t1.addComponent(2, 2);
+
+	Task t2 = new Task();
+	t2.addComponent(1, 2);
+	t2.addComponent(2, 2);
+
+	Task t3 = new Task();
+	t3.addComponent(2, 1);
+	t3.addComponent(1, 1);
+
+	Task t4 = new Task();
+	t4.addComponent(4, 1);
+
+	Task t5 = new Task();
+	t5.addComponent(5, 1);
+
+	s.addTask(t1);
+	s.addTask(t2);
+	s.addTask(t3);
+	s.addTask(t4);
+	s.addTask(t5);
+
+	// Start: [1, 2 | 3 || 4 | 5]
+	s.tick;
+	// -> [1, 2, 3 ||||] -> 4, 5
+	s.tick;
+	// -> [2, 3 | 1 |||]
+	s.tick;
+	// -> [2, 3 | 1 |||]
+	s.tick;
+	// -> [3 | 2 |||] -> 1
+	s.tick;
+	// -> [| 2 |||] -> 3
+	s.tick;
+	// -> [||||] -> 2
+	s.tick;
+	// -> [||||]
 }
